bbd45d463d5cf45d3b4427289dc968d7b3f990fe
Rannda <rch@connecthings.com>
2020-07-17 17:19:41 +0200

    Updates

---
+++ precache-manifest.ac0a1e8be8ff3c125ea8882f221443bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1240a5fe01a47b2463541c5bee2ba6b5",
+    "url": "/freestyle-app/index.html"
+  },
+  {
+    "revision": "f8523bb23d92ebe870c8",
+    "url": "/freestyle-app/static/css/main.d98e1378.chunk.css"
+  },
+  {
+    "revision": "659a85e1429e57fc01f6",
+    "url": "/freestyle-app/static/js/2.d252d98e.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/freestyle-app/static/js/2.d252d98e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8523bb23d92ebe870c8",
+    "url": "/freestyle-app/static/js/main.f5dbcfbb.chunk.js"
+  },
+  {
+    "revision": "9f4d794aff23e646123d",
+    "url": "/freestyle-app/static/js/runtime-main.afc250b7.js"
+  }
+]);
