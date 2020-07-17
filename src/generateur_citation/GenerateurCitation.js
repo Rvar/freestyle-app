63bae0bc8b76080d5fa1ba74ea9f72752103d386
Rvar <ranns95@gmail.com>
2020-07-17 19:24:09 +0200

    update citations

--- src/generateur_citation/GenerateurCitation.js
+++ src/generateur_citation/GenerateurCitation.js
@@ -3,14 +3,40 @@ import React, { Component } from "react";
 class GenerateurCitation extends Component {
 
   constructor(props, context) {
-    super(props, context);
+      super(props, context);
+
+      const data = require('./citations.json');
+      const listItems = data.map((item) =>
+        <div>
+          {item.citation}
+          <div> - {item.auteur}</div>
+        </div>
+      );
+
+      this.state = {citation: "",
+                    listItems: listItems}
+    }
+
+  pickACitation() {
+    const max = this.state.listItems.length - 1;
+
+    const rand = Math.floor(Math.random() * Math.floor(max))
+
+    console.log("random : " + rand);
+
+    this.setState({ citation: this.state.listItems[rand] });
   }
 
   render() {
     return (
       <div>
         <h2> GENERATEUR DE CITATION </h2>
-        <p> TODO </p>
+        <h3>
+          {this.state.citation}
+        </h3>
+        <a href="#" onClick={() => this.pickACitation()}>
+          Générer
+        </a>
       </div>
     );
   }
