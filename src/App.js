2179904598a98db6b9f570074d7f2da2b7a27990
rch <rannda.chea@herow.io>
2020-08-13 17:20:36 +0200

    todomatic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Home from './home/Home';
 import GenerateurCitation from './generateur_citation/GenerateurCitation';
@@ -14,55 +14,69 @@ import {
   Link
 } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-      <div className="menu">
-        <div>
-          <header className="App-header">
-            <h5>
-              <Link to="/">FREESTYLE - APP</Link>
-            </h5>
-          </header>
-           <ul>
-             <li>
-               <Link to="/generateurCitation">Générateur de citation</Link>
-             </li>
-             <li>
-               <Link to="/boiteAIdee">Boite à idées</Link>
-             </li>
-             <li>
-               <Link to="/todoList">Todo list</Link>
-             </li>
-             <li>
-               <Link to="/">autre</Link>
-             </li>
-           </ul>
+const DATATODO = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Rupture", completed: false },
+  { id: "todo-2", name: "Evenement", completed: false }
+];
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+        <div className="menu">
+          <div>
+            <header className="App-header">
+              <h5>
+                <Link to="/">FREESTYLE - APP</Link>
+              </h5>
+            </header>
+             <ul>
+               <li>
+                 <Link to="/generateurCitation">Générateur de citation</Link>
+               </li>
+               <li>
+                 <Link to="/boiteAIdee">Boite à idées</Link>
+               </li>
+               <li>
+                 <Link to="/todoList">Todo list</Link>
+               </li>
+               <li>
+                 <Link to="/">autre</Link>
+               </li>
+             </ul>
+           </div>
+        </div>
+        <div className="body">
+           <Switch>
+             //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+             //AJOUTER ROUTE ET LINK : Link to... (au dessus)
+             //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+             <Route path="/generateurCitation">
+               <GenerateurCitation />
+             </Route>
+             <Route path="/boiteAIdee">
+               <BoiteAIdee />
+             </Route>
+             <Route path="/todoList">
+               <TodoList tasks={DATATODO}/>
+             </Route>
+             <Route path="/">
+               <Home />
+             </Route>
+           </Switch>
          </div>
+        </Router>
       </div>
-      <div className="body">
-         <Switch>
-           //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-           //AJOUTER ROUTE ET LINK : Link to... (au dessus)
-           //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-           <Route path="/generateurCitation">
-             <GenerateurCitation />
-           </Route>
-           <Route path="/boiteAIdee">
-             <BoiteAIdee />
-           </Route>
-           <Route path="/todoList">
-             <TodoList />
-           </Route>
-           <Route path="/">
-             <Home />
-           </Route>
-         </Switch>
-       </div>
-      </Router>
-    </div>
-  );
+    );
+  }
 }
 
 
